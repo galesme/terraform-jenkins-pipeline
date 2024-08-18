6c3722dc4d724725a3f72b9e679eff629315404c
jackspades <galesme@yahoo.com>
2024-08-18 11:01:54 -0500

    s3 fix

--- s3.tf
+++ s3.tf
@@ -4,16 +4,25 @@ resource "aws_s3_bucket" "my_s3_bucket" {
   tags = var.tags
 }
 
-resource "aws_s3_bucket_acl" "s3_acl" {
-  #depends_on = [aws_s3_bucket_ownership_controls.my_s3_bucket]
-
-  bucket = aws_s3_bucket.my_s3_bucket.id
-  acl    = var.acl
-}
 
 resource "aws_s3_bucket_versioning" "s3_versioning" {
   bucket = aws_s3_bucket.my_s3_bucket.id
   versioning_configuration {
     status = "Enabled"
   }
+}
+####################
+
+resource "aws_s3_bucket_ownership_controls" "aws_s3_own" {
+  bucket = aws_s3_bucket.my_s3_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "aws_s3_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.aws_s3_own]
+
+  bucket = aws_s3_bucket.my_s3_bucket.id
+  acl    = var.acl
 }
