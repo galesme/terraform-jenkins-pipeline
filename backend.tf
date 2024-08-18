4d803c3d3a4d99209709e74f53e8b302fb6921f6
jackspades <galesme@yahoo.com>
2024-08-18 10:31:11 -0500

    ...

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "terraform-gitlab-cicd-pipeline-backend-galesme"
     key            = "tfstate"
     region         = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    dynamodb_table = "tf-jenkins-pipeline"
   }
 }
